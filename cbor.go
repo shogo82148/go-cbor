0e89666583cc5fe00284c635de13ffa6d8fb42be
ICHINOSE Shogo <shogo82148@gmail.com>
2023-09-17 23:04:00 +0900

    introduce Integer type

--- cbor.go
+++ cbor.go
@@ -1,6 +1,11 @@
 package cbor
 
-import "slices"
+import (
+	"errors"
+	"math"
+	"slices"
+	"strconv"
+)
 
 type undefined *struct{}
 
@@ -20,4 +25,52 @@ func (m RawMessage) MarshalCBOR() ([]byte, error) {
 func (m *RawMessage) UnmarshalCBOR(data []byte) error {
 	*m = slices.Clone(data)
 	return nil
+}
+
+// Integer is a CBOR integer type.
+type Integer struct {
+	// Sign is true if the integer is negative.
+	Sign bool
+
+	// Value presents the value of the integer.
+	// If the integer is positive, the value is Value itself.
+	// If the integer is negative, the value is -Value-1.
+	Value uint64
+}
+
+// Int64 returns the integer as int64.
+func (i Integer) Int64() (int64, error) {
+	if i.Sign {
+		v := int64(^i.Value)
+		if v >= 0 {
+			return 0, errors.New("cbor: integer overflow")
+		}
+		return v, nil
+	} else {
+		v := int64(i.Value)
+		if v < 0 {
+			return 0, errors.New("cbor: integer overflow")
+		}
+		return v, nil
+	}
+}
+
+// Uint64 returns the integer as uint64.
+func (i Integer) Uint64() (uint64, error) {
+	if i.Sign {
+		return 0, errors.New("cbor: integer overflow")
+	} else {
+		return i.Value, nil
+	}
+}
+
+func (i Integer) String() string {
+	if i.Sign {
+		if i.Value == math.MaxUint64 {
+			return "-18446744073709551616"
+		}
+		return "-" + strconv.FormatUint(i.Value+1, 10)
+	} else {
+		return strconv.FormatUint(i.Value, 10)
+	}
 }
