3ca26065701d2bbe1917446ef4dd2f7da8433da3
ICHINOSE Shogo <shogo82148@gmail.com>
2023-10-29 19:32:38 +0900

    introduce EncodeEDN

---
+++ edn.go
@@ -0,0 +1,77 @@
+package cbor
+
+import (
+	"bytes"
+	"math"
+	"strconv"
+)
+
+// ParseEDN parses the Extended Diagnostic Notation encoded data and returns the result.
+func ParseEDN(data []byte) (RawMessage, error) {
+	return nil, nil
+}
+
+// EncodeEDN returns the Extended Diagnostic Notation encoding of msg.
+func (m RawMessage) EncodeEDN() ([]byte, error) {
+	s := ednState{data: m}
+	s.encode()
+	if s.err != nil {
+		return nil, s.err
+	}
+	return s.buf.Bytes(), nil
+}
+
+type ednState struct {
+	buf  bytes.Buffer
+	data RawMessage
+	off  int // next read offset in data
+	err  error
+}
+
+func (s *ednState) readByte() (byte, error) {
+	if !s.isAvailable(1) {
+		return 0, ErrUnexpectedEnd
+	}
+	b := s.data[s.off]
+	s.off++
+	return b, nil
+}
+
+func (d *ednState) isAvailable(n uint64) bool {
+	if n > math.MaxInt {
+		// int(n) will overflow
+		return false
+	}
+	newOffset := d.off + int(n)
+	if newOffset < d.off {
+		// overflow
+		return false
+	}
+	return newOffset <= len(d.data)
+}
+
+func (s *ednState) encode() {
+	typ, err := s.readByte()
+	if err != nil {
+		s.err = err
+		return
+	}
+	switch typ {
+	// unsigned integer 0x00..0x17 (0..23)
+	case 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0a, 0x0b, 0x0c, 0x0d, 0x0e, 0x0f, 0x10, 0x11, 0x12, 0x13, 0x14, 0x15, 0x16, 0x17:
+		b := s.buf.AvailableBuffer()
+		b = strconv.AppendUint(b, uint64(typ), 10)
+		s.buf.Write(b)
+
+	// unsigned integer (one-byte uint8_t follows)
+	case 0x18:
+		w, err := s.readByte()
+		if err != nil {
+			s.err = err
+			return
+		}
+		b := s.buf.AvailableBuffer()
+		b = strconv.AppendUint(b, uint64(w), 10)
+		s.buf.Write(b)
+	}
+}
---
+++ edn_test.go
@@ -0,0 +1,40 @@
+package cbor
+
+import (
+	"testing"
+)
+
+func TestEncodeEDN(t *testing.T) {
+	tests := []struct {
+		in  RawMessage
+		out string
+	}{
+		{
+			in:  RawMessage{0x00},
+			out: "0",
+		},
+		{
+			in:  RawMessage{0x18, 0x18},
+			out: "24",
+		},
+		// {
+		// 	in: RawMessage{
+		// 		0xC0, // tag(0)
+		// 		0x74, // text(20)
+		// 		0x32, 0x30, 0x31, 0x33, 0x2D, 0x30, 0x33, 0x2D, 0x32, 0x31, 0x54, 0x32, 0x30, 0x3A, 0x30, 0x34, 0x3A, 0x30, 0x30, 0x5A,
+		// 	},
+		// 	out: `0("2013-03-21T20:04:00Z")`,
+		// },
+	}
+
+	for _, tt := range tests {
+		got, err := tt.in.EncodeEDN()
+		if err != nil {
+			t.Errorf("EncodeEDN() error = %v", err)
+			continue
+		}
+		if string(got) != tt.out {
+			t.Errorf("EncodeEDN() = %s, want %s", got, tt.out)
+		}
+	}
+}
